5625bc374f353ec765a82cff8a3256b604dde07e
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-02 22:32:15 -0300

    :wrench: Alterando app e index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.scss";
 import App from "./main/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 );
--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+
+import { lightTheme, darkTheme } from "./Styles/themes";
+import Router from "./Components/Route";
 
 function App() {
-  return <div className="App">Olá mundo!</div>;
+  const [theme, setTheme] = useState("light");
+
+  const themeSelect = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Header function={themeSelect} />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
