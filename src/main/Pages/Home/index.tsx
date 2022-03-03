c478b94e794d7c705368da9c608491dc0f3c9135
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-02 23:15:09 -0300

    :wrench: Adicionando função search e filter

--- src/main/Pages/Home/index.tsx
+++ src/main/Pages/Home/index.tsx
@@ -20,6 +20,43 @@ export default function HomePage() {
       const response = await fetch(API_URL);
       const data = await response.json();
       setDataAPI(data);
+
+      const search = document.getElementById("search") as HTMLInputElement;
+      const x = document.querySelectorAll(".cardItem") as NodeListOf<Element>;
+
+      search.addEventListener("input", () => {
+        let searc = search.value;
+        searc = searc.toLowerCase();
+
+        for (let i = 0; i < x.length; i++) {
+          if (!x[i].innerHTML.toLowerCase().includes(searc)) {
+            x[i].classList.add("remov");
+          } else {
+            x[i].classList.remove("remov");
+          }
+        }
+      });
+
+      const select1 = document.getElementById("filt") as HTMLSelectElement;
+
+      select1.addEventListener("change", () => {
+        let value1 = select1.options[select1.selectedIndex].value;
+        value1 = value1.toLowerCase();
+
+        if (value1 !== "all") {
+          for (let i = 0; i < x.length; i++) {
+            if (!x[i].innerHTML.toLowerCase().includes(value1)) {
+              x[i].classList.add("remov");
+            } else {
+              x[i].classList.remove("remov");
+            }
+          }
+        } else {
+          for (let i = 0; i < x.length; i++) {
+            x[i].classList.remove("remov");
+          }
+        }
+      });
     }
 
     restAPI();
@@ -36,27 +73,27 @@ export default function HomePage() {
             placeholder="Search for a country..."
             type="text"
             name=""
-            id=""
+            id="search"
           />
         </div>
 
-        <select name="" id="">
+        <select name="" id="filt">
           <option value="" disabled selected hidden>
             Filter by Region
           </option>
-          <option value="">All</option>
-          <option value="">Africa</option>
-          <option value="">America</option>
-          <option value="">Asia</option>
-          <option value="">Europe</option>
-          <option value="">Oceania</option>
+          <option value="All">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
         </select>
       </SearchContainer>
       <CardContainer>
         {dataAPI.length > 0 ? (
           dataAPI.map((val) => {
             return (
-              <CardWrapper key={val.name}>
+              <CardWrapper className="cardItem" key={val.name}>
                 <Link to={val.alpha3Code}>
                   <img src={val.flag} alt="" />
                   <div>
